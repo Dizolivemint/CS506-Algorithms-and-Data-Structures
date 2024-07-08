1c4cd687a82d2ba2c16bcd5096276354e57c260b
Dizolivemint <somabhi@gmail.com>
2024-07-08 13:33:42 -0700

    🚀 fixed local run

--- Portfolio/app/app.py
+++ Portfolio/app/app.py
@@ -151,4 +151,4 @@ if __name__ == '__main__':
         app.run(host="0.0.0.0", port=port)
     else:
         # Running locally
-        app.run(host="127.0.0.1", port=5000)
+        app.run(port=5000)
