0bdc9176466b16e239ae411477024129ecd3be9f
Dizolivemint <somabhi@gmail.com>
2024-07-09 20:21:09 -0700

    💄 switched webp for jpeg og

--- Portfolio/tsp-visualizer/src/components/head/index.tsx
+++ Portfolio/tsp-visualizer/src/components/head/index.tsx
@@ -13,7 +13,7 @@ const Head = () => {
       {/* Open Graph Meta Tags */}
       <meta property="og:title" content="Genetic Algorithm Visualization Tool" />
       <meta property="og:description" content="Explore the capabilities of genetic algorithms in solving the Traveling Salesman Problem with this interactive visualization tool." />
-      <meta property="og:image" content="https://cs-506-algorithms-and-data-structures.vercel.app/genetic-algorithm.webp" />
+      <meta property="og:image" content="https://cs-506-algorithms-and-data-structures.vercel.app/genetic-algorithm.jpg" />
       <meta property="og:url" content="https://cs-506-algorithms-and-data-structures.vercel.app" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Genetic Algorithm Visualization Tool" />
@@ -22,7 +22,7 @@ const Head = () => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Genetic Algorithm Visualization Tool" />
       <meta name="twitter:description" content="Explore the capabilities of genetic algorithms in solving the Traveling Salesman Problem with this interactive visualization tool." />
-      <meta name="twitter:image" content="https://cs-506-algorithms-and-data-structures.vercel.app/genetic-algorithm.webp" />
+      <meta name="twitter:image" content="https://cs-506-algorithms-and-data-structures.vercel.app/genetic-algorithm.jpg" />
       <meta name="twitter:site" content="@dizolivemint" />
       <meta name="twitter:creator" content="@dizolivemint" />
 
